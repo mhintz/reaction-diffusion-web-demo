5eef6b544f971e13284d487bf77e5c6c0b052eed
Mark Hintz <mark.hintz@datadoghq.com>
2021-10-21 17:05:43 -0400

    Build system

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import builtins from 'builtin-modules';
+import typescript from '@rollup/plugin-typescript';
+import glslify from 'rollup-plugin-glslify';
+// import resolve from 'rollup-plugin-node-resolve';
+// import commonjs from 'rollup-plugin-commonjs';
+
+export default {
+  input: 'reaction-diffusion.ts',
+  output: {
+    file: 'out/reaction-diffusion.js',
+    dir: 'out',
+    format: 'cjs',
+    sourcemap: true,
+    // homemade iife wrapper for the output bundle
+    banner: '(function() {\n',
+    footer: '\n})()',
+  },
+  external: [...builtins, 'electron'],
+  plugins: [
+    typescript({ module: 'es2015', outDir: 'out' }),
+    glslify(),
+    // resolve({
+    //   mainFields: ['module', 'main', 'browser'],
+    //   extensions: ['.ts', '.tsx', '.js'],
+    //   preferBuiltins: true,
+    // }),
+    // commonjs(),
+  ],
+};
